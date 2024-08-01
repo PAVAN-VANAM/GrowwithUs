c47b2ce1e55fdcd6c0cd3cb6ce4b3043ab0b7da4
Pavan Vanam <Vijaymohan8121@gmail.com>
2024-08-01 21:42:37 +0530

    LandingPage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Start from "./components/Start";
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <Start />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
